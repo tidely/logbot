2d79dcce6c2736dae5992d063c2c5b1816f4137c
tidely <43219534+tidely@users.noreply.github.com>
2024-12-31 19:13:14 +0200

    (logbot): Implement trait wrapper struct
    This implements a new wrapper struct called `Logbot`. It's purpose is allow passing of a single Sized struct at compile time which exports trait bounds. This makes it much easier have functions require specific traits for operation, without having to have a variable amount of arguments.

---
+++ crates/logbot/src/lib.rs
@@ -0,0 +1,97 @@
+//! The logbot crate provides a wrapper struct for all hardware components,
+//! which then exports interfaces as a single struct. This allows for easy
+//! trait bounds checking.
+
+use interfaces::{Drive, Lift, SensorRead, Spin, ToSensorChannel};
+use speed::Speed;
+
+/// Logbot struct that wraps all hardware components
+#[derive(Debug)]
+pub struct Logbot<V, S, L> {
+    vehicle: V,
+    sensors: S,
+    lift: L,
+}
+
+impl<V, S, L> Logbot<V, S, L> {
+    /// Create a new Logbot struct
+    pub fn new(vehicle: V, sensors: S, lift: L) -> Self {
+        Self {
+            vehicle,
+            sensors,
+            lift,
+        }
+    }
+}
+
+// Export Drive Trait for Logbot
+impl<V, S, L> Drive for Logbot<V, S, L>
+where
+    V: Drive,
+{
+    type Direction = V::Direction;
+    type Error = V::Error;
+
+    fn drive(
+        &mut self,
+        direction: Self::Direction,
+    ) -> Result<Option<Self::Direction>, Self::Error> {
+        self.vehicle.drive(direction)
+    }
+
+    fn stop(&mut self) -> Result<Option<Self::Direction>, Self::Error> {
+        self.vehicle.stop()
+    }
+}
+
+// Export Spin Trait for Logbot
+impl<V, S, L> Spin for Logbot<V, S, L>
+where
+    V: Spin,
+{
+    type SpinDirection = V::SpinDirection;
+
+    fn spin(
+        &mut self,
+        direction: Self::SpinDirection,
+    ) -> Result<Option<Self::Direction>, Self::Error> {
+        self.vehicle.spin(direction)
+    }
+}
+
+// Export SensorRead Trait for Logbot
+impl<V, S, L> SensorRead for Logbot<V, S, L>
+where
+    S: SensorRead,
+{
+    type Output = S::Output;
+    type Error = S::Error;
+
+    fn read(&mut self, sensor: impl ToSensorChannel) -> Result<Self::Output, Self::Error> {
+        self.sensors.read(sensor)
+    }
+}
+
+// Export Lift Trait for Logbot
+impl<V, S, L> Lift for Logbot<V, S, L>
+where
+    L: Lift,
+{
+    type Error = L::Error;
+
+    fn up(&mut self, speed: Speed) -> Result<(), Self::Error> {
+        self.lift.up(speed)
+    }
+
+    fn down(&mut self, speed: Speed) -> Result<(), Self::Error> {
+        self.lift.down(speed)
+    }
+
+    fn is_up(&self) -> bool {
+        self.lift.is_up()
+    }
+
+    fn is_down(&self) -> bool {
+        self.lift.is_down()
+    }
+}
